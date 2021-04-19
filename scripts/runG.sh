7e0abb7f7c332321b2cabd912bdc3613b61a202c
Alexandre Bergel <alexandre.bergel@me.com>
2021-04-19 15:15:14 +0200

    Update runG.sh

--- scripts/runG.sh
+++ scripts/runG.sh
@@ -6,9 +6,9 @@ cp ../scripts/runG.st .
 
 cat /tmp/result.txt
 if grep -q ERROR "/tmp/result.txt"; then
-	echo "SOME ERROR WERE FOUND"
+	echo "Errors: Some tests failed"
 	exit 1
 else
-	echo "ALL TEST PASSED"
+	echo "Passed: all the tests passed"
 	exit 0
 fi		
